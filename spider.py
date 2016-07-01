f6a25b42a80df7b004500905510b410eb58bd472
zhouyifan <552094467@qq.com>
2016-07-01 18:30:00 +0800

    add download

--- spider.py
+++ spider.py
@@ -1,4 +1,8 @@
+import queue
+import threading
+import requests
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
 
 class Route(object):
@@ -7,10 +11,8 @@ class Route(object):
 
     def add(self, url, func):
         node = self.root
-        print(node)
 
         parse_result = urlparse(url)
-        print(parse_result.path)
         urls = [url for url in parse_result.path.split('/') if url]
         print(urls)
         if len(urls) == 0:
@@ -60,23 +62,67 @@ class Node(object):
     def __str__(self):
         return str(self.sub_node)
 
+class Task(object):
+    def __init__(self, type, url=None):
+        self.type = type
+        self.url = url
+        self.result = None
+
+class Download(threading.Thread):
+    def __init__(self, url, spider):
+        super().__init__()
+        self.url = url
+        self.spider = spider
+
+    def run(self):
+        r = requests.get(self.url)
+        if r.status_code == 200:
+            parse_result = urlparse(r.url)
+            soup = BeautifulSoup(r.text, "lxml")
+
+            def convert(href):
+                href_result = urlparse(href)
+
+                if href_result.scheme != '':
+                    return href_result.geturl()
+                elif href_result.netloc != '':
+                    return parse_result.scheme + "://" + href_result.geturl()
+                else:
+                    return parse_result.scheme + "://" + parse_result.netloc + href_result.geturl()
+
+            if self.spider != None:
+                for url in [convert(a['href']) for a in soup.select('a') if a['href'] != 'javascript:;']
+                    task = new Task("parse", url)
+                    task.result = r.text
+                    self.spider.task_queue(task)
+
 
 class Spider(object):
-    def __init__(self):
+    def __init__(self, start_url):
         self.r = Route()
+        self.task_queue = queue.Queue()
+        self.url_queue.put(Task('download', start_url))
 
     def route(self, url):
         print(url)
 
         def _deco(func):
             self.r.add(url, func)
-
             def __deco(*args, **kwargs):
                 func(*args, **kwargs)
-
             return __deco
-
         return _deco
 
     def run(self):
-        pass
+        while True:
+            task = self.task_queue.get()
+
+            if task.type == 'download':
+                Download(task.url, self).start()
+            elif task.type == 'parse':
+                self.r.search(task.url)()
+
+
+if __name__ == '__main__':
+    download = Download("http://www.baidu.com", None)
+    download.start()
