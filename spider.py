e5288141c5df5e3511556341c9045f94c408caa2
zhouyifan <552094467@qq.com>
2016-07-07 18:06:41 +0800

    modify route part

--- spider.py
+++ spider.py
@@ -5,6 +5,7 @@ import requests
 import re
 import redis
 import pickle
+import time
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 
@@ -49,20 +50,21 @@ class Route(object):
         while len(node.sub_node) > 0 and len(urls) > i:
             sub_url = urls[i]
             i += 1
-
             for item in node.sub_node.values():
-                if item == sub_url:
-                    node = item
-                    if item.param_type == 'int':
-                        args[item.param_name] = int(sub_url)
-                    elif item.param_type == 'string':
-                        args[item.param_name] = sub_url
-                    elif item.param_type == 'float':
-                        args[item.param_name] = float(sub_url)
-                    break
+                if item.param_type == 'base':
+                    if item.name != sub_url:
+                        continue
+                else:
+                    value = item.get_value(sub_url)
+                    if value is None:
+                        continue
+                    args[item.param_name] = value
+                node = item
+                break
             else:
                 node = None
                 break
+
         if node is None or i < len(urls):
             return None, args
         else:
@@ -79,7 +81,7 @@ class Route(object):
         return str(self.root.sub_node)
 
 
-param_re = re.compile('^<(int|string|float):([a-zA-Z_]\w+)>$')
+param_re = re.compile('<(int|string|float):([a-zA-Z_]\w+)>')
 
 
 class Node(object):
@@ -92,40 +94,48 @@ class Node(object):
         if result:
             self.param_type = result.group(1)
             self.param_name = result.group(2)
+
+            if self.param_type == 'int':
+                self.pattern = re.sub('<int:\w+>', '([+,-]{0,1}\d+)', self.name)
+            elif self.param_type == 'string':
+                self.pattern = re.sub('<string:\w+>', '\w+', self.name)
+            elif self.param_type == 'float':
+                self.pattern = re.sub('<float:\w+>', '([+,-]{0,1}\d+.{0,1}\d+)', self.name)
         else:
             self.param_type = 'base'
 
     def add(self, node):
         self.sub_node[node.name] = node
 
+    def get_value(self, sub_url):
+        result = re.match(self.pattern, sub_url)
+
+        funcs = {
+            'int': lambda result: int(result.group(1)),
+            'string': lambda result: result.group(1),
+            'float': lambda result: float(result.group(1))
+        }
+
+        if result is None:
+            return None
+        return funcs[self.param_type](result)
+
     def __str__(self):
         return '[' + self.name + ' ' + str(self.sub_node) + ']'
 
 
-    def __eq__(self, other):
-        if self.param_type == 'base':
-            return other == self.name
-        elif self.param_type == 'string':
-            return True
-        elif self.param_type == 'int':
-            return (True if int_number.match(other) else False)
-        elif self.param_type == 'float':
-            return (True if float_number.match(other) else False)
-        else:
-            return False
-
-
-class Request(object):
+class Response(object):
     def __init__(self):
         self.req = {}
 
-    def get_request(self):
+    def get_response(self):
         return self.req[threading.current_thread().ident]
 
-    def add_request(self, pid, req):
+    def add_response(self, pid, req):
         self.req[pid] = req
 
-request = Request()
+
+response = Response()
 
 
 class Worker(threading.Thread):
@@ -139,12 +149,14 @@ class Worker(threading.Thread):
                 # 记得错误处理
                 return
 
-            tmp = self.spider.redis.rpop('task_queue')
+            tmp = self.spider.pop_task()
             if tmp is None:
                 continue
             task = pickle.loads(tmp)
             url = task.url
 
+            # todo 标记该网页已经被爬过
+            self.spider.redis.set(url, True)
             r = requests.get(url)
             if r.status_code != 200:
                 pass
@@ -160,21 +172,19 @@ class Worker(threading.Thread):
                 if sub_url.startswith('https'):
                     continue
 
-                sub_task = Task(sub_url)
-
                 # 这个地方还需要修改，去除不必要的url，从route里面找
                 # if not self.spider.r.search(sub_url):
                 #     continue
                 if self.spider.redis.exists(sub_url) and not self.spider.redis.get(sub_url):
                     continue
-                self.spider.redis.lpush('task_queue', pickle.dumps(sub_task))
-                self.spider.redis.set(sub_url, False)
 
-            # todo 调用相关的网页处理函数，标记该网页已经被爬过
+                sub_task = Task(sub_url)
+                self.spider.push_task(sub_task)
+
             func, args = self.spider.r.get_func(url)
             if func is None:
                 continue
-            request.add_request(threading.get_ident(), r)
+            response.add_response(threading.get_ident(), r)
             func(**args)
 
     def convert(self, href, url):
@@ -215,29 +225,45 @@ class Spider(object):
         self.redis.flushall()
 
         task = Task(start_url)
-        self.redis.lpush('task_queue', pickle.dumps(task))
-        self.redis.set(start_url, False)
+        self.push_task(task)
 
         self.config = Config()
-
 
     def route(self, url):
         def _deco(func):
             self.r.add(url, func)
+
         return _deco
 
     def run(self):
         for i in range(self.config.get('worker')):
             Worker(self).start()
 
+    def push_task(self, task):
+        mutex = threading.Lock()
+        mutex.acquire()
+        if self.r.search(task.url):
+            self.redis.rpush('task_queue', pickle.dumps(task))
+        else:
+            self.redis.lpush('task_queue', pickle.dumps(task))
+        self.redis.set(task.url, False)
+        mutex.release()
 
-spider = Spider('http://haha.sogou.com')
+    def pop_task(self):
+        return self.redis.rpop('task_queue')
 
-@spider.route('/<int:id>')
+
+spider = Spider('http://www.mahua.com/xiaohua/1628976.htm')
+
+
+@spider.route('/xiaohua/<int:id>.htm')
 def test(id):
-    result = request.get_request()
-    soup = BeautifulSoup(result.text)
-    print(id, soup.select('h2'))
+    result = response.get_response()
+    soup = BeautifulSoup(result.text, "lxml")
+    now = time.strftime('[%Y-%m-%d %H:%M:%S]',time.localtime(time.time()))
+    print(now, id, threading.current_thread().ident, soup.select('h1'))
+    with open('log.txt', 'a') as f:
+        f.write('{0} {1} {2} {3}\n'.format(now, id, threading.current_thread().ident, soup.select('h1')))
 
 
 if __name__ == '__main__':
